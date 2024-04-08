3d8f997b9e7e1e4883eeb77bc88859d8bddfe24e
Damian Lobos <damianlobos24@gmail.com>
2024-04-08 10:48:54 -0300

    Change the script to work with rigidbody

--- Space-Time Patrol/Assets/Scripts/PlayerController.cs
+++ Space-Time Patrol/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
+    private Transform playerPosition;
+    private Rigidbody rbPlayer;
     private Transform playerBody;
     private Vector3 moveDirection;
     private float speedMovement = 10f;
     private float rotationSpeed = 10f;
+    private float jumpForce = 5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        playerPosition = GetComponent<Transform>();
+        rbPlayer = GetComponent<Rigidbody>();
         playerBody = GetComponentInChildren<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
         moveDirection = new Vector3(horizontal, 0, vertical);
-        characterController.Move(moveDirection * speedMovement * Time.deltaTime);
-        if(moveDirection != Vector3.zero)
+        if (moveDirection != Vector3.zero)
         {
             Quaternion playerBodyRotation = Quaternion.LookRotation(moveDirection);
             playerBody.rotation = Quaternion.Slerp(playerBody.rotation, playerBodyRotation, rotationSpeed * Time.deltaTime);
         }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        rbPlayer.MovePosition(playerPosition.position + moveDirection * speedMovement * Time.deltaTime);
     }
 }
