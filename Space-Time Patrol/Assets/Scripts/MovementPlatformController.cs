e5e6f9ebbf34b0408be1adc19dd4f2cc4bed335b
Damian Lobos <damianlobos24@gmail.com>
2024-04-09 14:10:39 -0300

    Reorganized code

--- Space-Time Patrol/Assets/Scripts/MovementPlatformController.cs
+++ Space-Time Patrol/Assets/Scripts/MovementPlatformController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MovementPlatformController : MonoBehaviour
 {
@@ -18,21 +19,23 @@ public class MovementPlatformController : MonoBehaviour
     {
         if (!reachedPoint)
         {
-            Vector3 targetPosition = transform.position + Vector3.right * speedMovement * Time.deltaTime;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
-            rbPlatform.MovePosition(smoothedPosition);
+            MovePlatform(Vector3.right);
         }
         else
         {
-            Vector3 targetPosition = transform.position + Vector3.left * speedMovement * Time.deltaTime;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
-            rbPlatform.MovePosition(smoothedPosition);
+            MovePlatform(Vector3.left);
         }
+    }
+
+    private void MovePlatform(Vector3 direction)
+    {
+        Vector3 targetPosition = transform.position + direction * speedMovement * Time.deltaTime;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
+        rbPlatform.MovePosition(smoothedPosition);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         reachedPoint = !reachedPoint;
     }
-
 }
