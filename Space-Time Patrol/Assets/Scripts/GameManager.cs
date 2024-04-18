5928d334c9ac6e90d47733f01da2b6eb87283af3
Damian Lobos <damianlobos24@gmail.com>
2024-04-18 13:31:24 -0300

    cursor locked and falldeath amplied

--- Space-Time Patrol/Assets/Scripts/GameManager.cs
+++ Space-Time Patrol/Assets/Scripts/GameManager.cs
@@ -18,6 +18,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         instance = this;
         playerController = FindObjectOfType<PlayerController>();
         playerController.PlayerLifes = 3;
